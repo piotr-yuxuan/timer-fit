100bf34359fb7a505c8871307053227c7b6bd5c1
piotr-yuxuan <pierre2b@adikteev.com>
2016-08-28 20:25:01 +0200

    Material UI components and icons

--- project.clj
+++ project.clj
@@ -10,7 +10,9 @@
                  [org.clojure/clojurescript "1.9.89"]
                  [org.clojure/core.async "0.2.385"
                   :exclusions [org.clojure/tools.reader]]
-                 [reagent "0.5.1"]]
+                 [reagent "0.6.0-rc" :exclusions [org.clojure/tools.reader
+                                                  cljsjs/react]]
+                 [cljs-react-material-ui "0.2.21"]]
 
   :plugins [[lein-figwheel "0.5.4-7"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
--- src/project/core.cljs
+++ src/project/core.cljs
@@ -1,5 +1,9 @@
 (ns project.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljsjs.material-ui]
+            [cljs-react-material-ui.core :as ui]
+            [cljs-react-material-ui.reagent :as rui]
+            [cljs-react-material-ui.icons :as ic]))
 
 (enable-console-print!)
 
@@ -9,10 +13,28 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(defn home-page []
+  [rui/mui-theme-provider
+   {:mui-theme (ui/get-mui-theme
+                {:palette {:text-color (ui/color :green600)}})}
+   [:div
+    [rui/app-bar {:title              "Title"
+                  :icon-element-right (ui/icon-button
+                                       (ic/action-account-balance-wallet))}]
+    [rui/paper
+     [:div "Hello"]
+     [rui/mui-theme-provider
+      {:mui-theme (ui/get-mui-theme {:palette {:text-color (ui/color :blue200)}})}
+      [rui/raised-button {:label "Blue button"}]]
+     (ic/action-home {:color (ui/color :grey600)})
+     [rui/raised-button {:label        "Click me"
+                         :icon         (ic/social-group)
+                         :on-touch-tap #(println "clicked")}]]]])
+
 (defn hello-world []
   [:h1 (:text @app-state)])
 
-(reagent/render-component [hello-world]
+(reagent/render-component [home-page]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
